e3c1a8fa370e6f28093e3700b218df84e2a57109
wdeeee <tranquocdectk1821@gmail.com>
2024-05-19 01:04:09 +0700

    create sprite and movement for vehicles

---
+++ delivery-boy/Assets/Scripts/Vertical_vehicle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vertical_vehicle : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed;
+
+    private Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         myBody.velocity = new Vector2(myBody.velocity.x , speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Detect Collision");
+
+    }
+
+}
