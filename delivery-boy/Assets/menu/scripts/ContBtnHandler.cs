01de83930af9acb99877443af700ab37129eb8aa
wdeeee <tranquocdectk1821@gmail.com>
2024-06-07 07:28:22 +0700

    update scenes

---
+++ delivery-boy/Assets/menu/scripts/ContBtnHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContBtnHandler : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject pauseBtn;
+
+    public GameObject joystick;
+    // public GameObject taskList;
+    //public HealthBar healthBar;
+    public GameObject itemDelivered;
+    public CountdownTimer timer;
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        // taskList.SetActive(true);
+        pauseBtn.SetActive(true);
+        joystick.SetActive(true);
+        //healthBar.gameObject.SetActive(true);
+        itemDelivered.SetActive(true);
+        timer.counting = true;
+    }
+}
---
+++ delivery-boy/Assets/menu/scripts/backMenuBtn.cs
@@ -0,0 +1,20 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class backMenuBtn : MonoBehaviour
+//{
+//    public GameObject currentPanel;
+
+//    public GameData data;
+//    public void backToMenu()
+//    {
+//        data = SaveSystem.Load();
+//        currentPanel.SetActive(false);
+//    }
+
+//    public void exit()
+//    {
+//        Application.Quit();
+//    }
+//}
