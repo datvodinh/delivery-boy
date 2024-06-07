f9cf12adc94fb91f59f8a10117281288f7775144
wdeeee <tranquocdectk1821@gmail.com>
2024-06-07 14:40:45 +0700

    update

---
+++ delivery-boy/Assets/TaskExit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskExit : MonoBehaviour
+{
+    public void exitTask()
+    {
+        //SceneManager.LoadScene(0);
+        Debug.Log("Exit the game");
+    }
+}
