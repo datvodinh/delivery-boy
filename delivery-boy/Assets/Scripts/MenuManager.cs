4841f4639bee1bdf3cf614e42e9b74b9fd8c108e
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-06-10 16:39:01 +0700

    connect pause, menu, main

--- delivery-boy/Assets/Scripts/MenuManager.cs
+++ delivery-boy/Assets/Scripts/MenuManager.cs
@@ -5,15 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private string mainSceneName = "main";
+    private string mainMenuSceneName = "main_menu";
+    private string pauseScreenName = "pause_screen";
 
-    public int gameStartScene;
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync("main");
+        SwitchScene(mainSceneName);
+        Time.timeScale = 1f; // Resume the game
+    }
+
+    public void MainMenu()
+    {
+        SwitchScene(mainMenuSceneName);
+    }
+
+    public void ResumeGame()
+    {
+        StartCoroutine(UnloadPauseScreen());
+        Time.timeScale = 1f; // Resume the game
+    }
+
+    private IEnumerator UnloadPauseScreen()
+    {
+        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(pauseScreenName);
+        while (!asyncUnload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void PauseGame()
+    {
+        SceneManager.LoadScene(pauseScreenName, LoadSceneMode.Additive);
+        Time.timeScale = 0f; // Pause the game
     }
 
     public void QuitGame()
     {
         Application.Quit();
-    }    
+    }
+
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
