6101a5d07e7ca574154ed3b06ac788d5365506be
wdeeee <tranquocdectk1821@gmail.com>
2024-06-10 18:03:29 +0700

    fix collector and vehicle mechanism

--- delivery-boy/Assets/Scripts/Vehicles/Horizontal_vehicle.cs
+++ delivery-boy/Assets/Scripts/Vehicles/Horizontal_vehicle.cs
@@ -34,7 +34,7 @@ public class Horizontal_vehicle : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Vehicle"))
         {
             //Debug.Log("Detect vehicle Collision");
             //StartCoroutine(StopMoveRoutine());
